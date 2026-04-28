4072904c92378ea136a961f0f76caa7dd655286d
okshanaby <steamnature006@gmail.com>
2026-04-28 21:31:01 +0300

    Adding Dependencies (npm install) to Docker

--- my-project/index.js
+++ my-project/index.js
@@ -1,10 +1,17 @@
-const http = require("http");
+// this is the sample app from fastify.dev
 
-http
-  .createServer(function (request, response) {
-    console.log("request received");
-    response.end("omg hi", "utf-8");
-  })
-  .listen(3000);
+// Require the framework and instantiate it
+const fastify = require("fastify")({ logger: true });
 
-console.log("server started");
+// Declare a route
+fastify.get("/", function handler(request, reply) {
+  reply.send({ hello: "world" });
+});
+
+// Run the server!
+fastify.listen({ port: 8080, host: "0.0.0.0" }, (err) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+});
